757c95317eea2b85fa446d85ddfed16ee0f992b4
Alexander Bleissem <alexander@bleissem.com>
2023-06-27 22:55:01 +0200

    use clang dep

--- .github/clang.sh
+++ .github/clang.sh
@@ -5,6 +5,6 @@
 # wget https://apt.llvm.org/llvm.sh
 # chmod +x llvm.sh
 # sudo ./llvm.sh 14
-sudo apt-get install -y libapr1-dev libomp-14-dev
+sudo apt-get install -y libapr1-dev libomp-14-dev  build-essential libstdc++6 libc6
 # sudo ln -sf /usr/bin/clang-14 /usr/bin/clang
 # clang --version
